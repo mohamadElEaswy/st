99f13f0ebaf09569046d227c846ee1afed60a1e6
mohamed easwy <muhamadeleaswy535@gmail.com>
2022-01-07 21:41:58 +0200

    +1 co

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:st/src/services/navigation/navigation.dart';
-import 'package:st/src/services/navigation/navigation_methods.dart';
+import 'package:st/src/core/navigation/navigation_methods.dart';
 import 'package:st/src/ui/pages/home/home_page.dart';
 import 'package:st/src/ui/theme/theme.dart';
 
@@ -18,8 +17,6 @@ class MyApp extends StatelessWidget {
       title: title,
       theme: AppTheme.lightTheme,
       home: const Home(title: title),
-      // routes: NavigationRouts.routes,
-      // initialRoute: NavigationRouts.home,
       onGenerateRoute: RouteMethods.generateRoutes,
       initialRoute: Home.route,
     );
